7d77b0b399951d7ee3a78423d01c1653e2d41181
andre.oliveira <andre.oliveira@netshoes.com>
2019-08-08 16:59:34 -0300

    compose

---
+++ src/main/java/com/hedera/hedera/gateway/http/SellerController.java
@@ -0,0 +1,32 @@
+package com.hedera.hedera.gateway.http;
+
+import com.hedera.hedera.entitiy.Seller;
+import com.hedera.hedera.usecase.SellerManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
+@RestController("/api/seller/")
+@RequiredArgsConstructor
+public class SellerController {
+
+    private final SellerManager sellerManager;
+
+    @PostMapping(
+            produces = {APPLICATION_JSON_VALUE})
+    public ResponseEntity<Seller> createSeller(Seller seller) {
+        return ResponseEntity.ok(sellerManager.createSelller(seller));
+    }
+
+//    @GetMapping(
+//            produces = {APPLICATION_JSON_VALUE})
+//    public ResponseEntity<Long> getAccountSeller(Seller seller) {
+//        return ResponseEntity.ok(sellerManager.createSelller(seller));
+//    }
+
+}
