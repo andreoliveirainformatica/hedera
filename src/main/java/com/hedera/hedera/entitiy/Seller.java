19fdc2378e35a0192b00a44cac95a88c96c988cb
andre.oliveira <andre.oliveira@netshoes.com>
2019-08-08 17:29:16 -0300

    compose

--- src/main/java/com/hedera/hedera/entitiy/Seller.java
+++ src/main/java/com/hedera/hedera/entitiy/Seller.java
@@ -3,7 +3,6 @@ package com.hedera.hedera.entitiy;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -14,12 +13,11 @@ import java.math.BigDecimal;
 @Document
 public class Seller {
 
-    @Id
     private String id;
 
     private String nome;
 
-    private BigDecimal commisisonPercent;
+    private BigDecimal commissionPercent;
 
     private String accountId;
 }
